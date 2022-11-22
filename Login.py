ed2cee3237f2021066bd93eb70c5c65f4fbe1613
TheGag2000 <epictank2000@hotmail.co.uk>
2022-11-22 16:16:39 +0000

    A level project

--- Login.py
+++ Login.py
@@ -1,1 +1,6 @@
 import tkinter as tk
+class LoginWindow(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        test = tk.Label(self, text = "test Window")
+        test.pack()
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import tkinter as tk #imports the tkinter library to bo be used to create GUI
+from Login import LoginWindow # imports the login window
+from RevisionWindow import TestWindow
+
 
 #RevisionApp class is used to configure application to allow for OOP windows
-
-class LoginWindow(tk.Frame):
-    def __init__(self, parent, container):
-        tk.Frame.__init__(self,parent)
-        test = tk.Label(self, text = "test Window")
-        test.pack()
-
 class RevisionApp(tk.Tk): #RevisionApp class inherits from tk.Tk
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs) #initialises tkinter to be used within the revisionap class
@@ -16,13 +12,13 @@ class RevisionApp(tk.Tk): #RevisionApp class inherits from tk.Tk
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
         self.frames = {}
 
-        frame = LoginWindow(container, self)
+        for F in (LoginWindow, TestWindow):
 
-        self.frames[LoginWindow] = frame
-        frame.grid(row=0, column = 0, sticky = "nsew")
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column = 0, sticky = "nsew")
 
         self.show_frame(LoginWindow)
 
@@ -31,7 +27,7 @@ class RevisionApp(tk.Tk): #RevisionApp class inherits from tk.Tk
         frame.tkraise()
 
 
-
-app = RevisionApp()
-app.mainloop()
+if __name__ == "__main__":
+    app = RevisionApp()
+    app.mainloop()
 
